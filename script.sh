10837d3d2d497b114b4cf696f1e08dfe49025f87
WASPEl2 <aekapab10@gmail.com>
2023-05-21 02:03:26 +0700

    ready deploy maybe v5

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Build your application (if necessary)
+# <insert build commands here>
+
+# Start your Python application
+python app.py
