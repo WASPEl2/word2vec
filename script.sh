fba8c189f57083f7c0a9efed86cfdd1a01813bf4
WASPEl2 <aekapab10@gmail.com>
2023-05-21 02:15:09 +0700

    ready deploy maybe v6.3

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@
 pip install -r requirements.txt
 
 # Install Flask (if not already installed)
-pip install flask
+# pip install flask
 
 # Build your application (if necessary)
 # <insert build commands here>
 
 # Start your Python application
-python app.py
+# python app.py
